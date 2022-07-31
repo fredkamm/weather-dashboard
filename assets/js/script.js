c6d8dbbb774ba3fecc7b3f22ceda185e69221e28
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-07-31 17:45:52 -0400

    input now gets added to local storage, going to try to add search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// Psuedocode 
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
 var apiKey = "2dee45951d3010a3a0e002fc582e7cc5";
 
 // sections from the document
@@ -14,7 +27,7 @@ var citySubmitHandler = function (event) {
 
     if (cityName) {
         getCityRepos(cityName);
-        get5Day(cityName);
+
     }
 }
 
@@ -49,7 +62,14 @@ var getCityRepos = function () {
             cityResults.append(displayWind);
             cityResults.append(displayHumid);
             cityResults.append(displayUVI);
-            cityResults.setAttribute('style', "border: 1px solid black;")
+            cityResults.setAttribute('style', "border: 1px solid black;");
+        })
+        // saves the city to local storage
+        .then(function (save) {
+            localStorage.setItem('city', JSON.stringify(cityInput.value.trim()));
+        })
+        .then(function (get){
+            
         })
 }
 
